b55ce869a5787eb07c3820d203367f614d69a082
CharlyTB30 <carlos_josi@hotmail.com>
2022-06-01 16:30:46 -0500

    prueba

---
+++ resources/js/components/carrusel/carrusel.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1>Catalogo</h1>
+    <div>
+            <img src="../../../../public/images/libro1.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro2.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro3.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro4.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro5.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro6.jpg" width="220" height="300">            
+    </div>
+
+    <pre>
+    - El Amor Molesto -              - La trenza -                       - Paula -                  - Nueve Lunas -                 - Guia Para Madres -            - Parece que Fuera -
+                                                                                                                                         Rebeldes                        es Primera
+
+        S/. 69.00                      S/. 59.00                         S/. 39.00                    S/. 59.00                         S/. 39.00                         S/. 89.00
+    
+
+    </pre>
+    <h1>Destacado</h1>
+    <div>
+            <img src="../../../../public/images/libro7.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro8.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro9.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro10.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro11.jpg" width="220" height="300">
+            <img src="../../../../public/images/libro12.jpg" width="220" height="300">            
+    </div>
+    <pre>
+   - El Año del Viento -            - Orgullo y Prejuicio -           - Las Tres Hermas -            - Una Habitacion -              - Julia Quinn -                  - El Infinito -
+                                                                                                           Propia                                                       en un Junco
+
+       S/. 69.00                          S/. 59.00                       S/. 39.00                      S/. 59.00                      S/. 39.00                        S/. 89.00
+    
+
+    </pre>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
